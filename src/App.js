9de5e16e267bb9971a21d812683ecf7b2b849622
Mohamed Yasar <99091486+mdyasar49@users.noreply.github.com>
2024-11-08 17:03:27 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Calculator from './Components/Calculator.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Calculator/>
+    </div>
+  );
+}
+
+export default App;
